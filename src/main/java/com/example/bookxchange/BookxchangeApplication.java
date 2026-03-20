66a1daff71a6b8d6c0d8983863ea66dd473c98a9
asiqueehety <asique228@gmail.com>
2026-03-20 06:53:40 +0600

    Initial commit

---
+++ src/main/java/com/example/bookxchange/BookxchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookxchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookxchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookxchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookxchange/BookxchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookxchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookxchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
