4309900d07d4fe62ef83fef1dfe1be6624ea45f6
TAJNOOR SULTANA <sultana2107108@stud.kuet.ac.bd>
2026-04-03 22:21:37 +0600

    Updated testing properties

--- src/test/java/com/example/bookxchange/BookxchangeApplicationTests.java
+++ src/test/java/com/example/bookxchange/BookxchangeApplicationTests.java
@@ -3,9 +3,11 @@ package com.example.bookxchange;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class BookxchangeApplicationTests {
 
 	@Test
